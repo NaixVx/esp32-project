90448b4149403701df6455f560a9e8ae0870bd51
NaixVx <adamparkitny.dev@gmail.com>
2025-08-03 15:53:04 +0200

    feat: init nvs before tests

--- test/main/example_unit_test.c
+++ test/main/example_unit_test.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 
+#include "nvs_flash.h"
 #include "unity.h"
 
 #ifdef __cplusplus
@@ -25,6 +26,15 @@ TEST_CASE("Config manager basic test", "[config_manager]") {
 }
 
 void app_main(void) {
+    // init nvs
+    esp_err_t ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+        ESP_ERROR_CHECK(nvs_flash_erase());
+        ret = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK(ret);
+
+    // tests
     UNITY_BEGIN();
     unity_run_all_tests();
     UNITY_END();
